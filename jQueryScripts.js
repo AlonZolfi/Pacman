9be6da88bbd639c826499fd34636c349fa33be4f
AlonZolfi <alonzolfi44@gmail.com>
2019-04-26 23:37:51 +0300

    added navigation between menu elements

--- jQueryScripts.js
+++ jQueryScripts.js
@@ -1,3 +1,12 @@
-$("a_welcome").click(function(){
-    $("welcome")
+$(".a_menu").click(function(event){
+    var div_content = $("#content > div");
+    var clickedName = this.id;
+    for (let i = 0; i < div_content.length; i++) {
+        var divName = div_content[i].id;
+        var shortName = clickedName.substring(2,clickedName.length);
+        if(divName === shortName)
+            div_content[i].hidden = false;
+        else
+            div_content[i].hidden = true;
+    }
 });
